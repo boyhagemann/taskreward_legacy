a51b89abbb85945413e1a08306911075cce8ec16
boyhagemann <boyhagemann@gmail.com>
2014-06-27 10:03:53 +0200

    update install command

--- app/commands/InstallCommand.php
+++ app/commands/InstallCommand.php
@@ -38,13 +38,13 @@ class InstallCommand extends Command {
 	public function fire()
 	{
         if(Schema::hasTable('migrations')) {
-            $this->call('migrate:refresh', ['--force' => null]);
-            $this->call('db:seed', ['--force' => null]);
+            $this->call('migrate:refresh', ['--force' => '--force']);
+            $this->call('db:seed', ['--force' => '--force']);
         }
         else {
             $this->call('migrate', ['--package' => 'cartalyst/sentry']);
             $this->call('migrate');
-            $this->call('db:seed', ['--force' => null]);
+            $this->call('db:seed', ['--force' => '--force']);
         }
 
 	}
